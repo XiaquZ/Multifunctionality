6702ec7d38c9e9d7a13b4ecc3f1571a7516406e9
XiaquZ <xiaqu.zhou@gmail.com>
2024-05-24 11:48:41 +0200

    R scripts example for beta regression with sptial autocorrelation.

---
+++ Regression/BetaR_example.R
@@ -0,0 +1,13 @@
+library(mgcv)
+####Use 'gam()' for faster beta regression accounting spatial autocorrelation.####
+#beta without intercept.
+mod_beta <- gam(
+  MF_av ~ type + coast + cover + elevation + eastness +
+    northness + relative_elevation + slope + TWI +
+    s(x, y, bs = "gp", m = 2) - 1, 
+    #"x", "y" refer to longtitude and latitude. "gp" means Gussian process
+    #if want to include the intercept in the results, remove '-1'.
+  family = betar(link = "logit"),
+  data = mydata
+)
+summary(mod_beta)
