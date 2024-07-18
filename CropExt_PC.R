97e7506a0579162b96e14bdfcd4acb8b10d3c169
Xiaqu Zhou <u0142858@kuleuven.be>
2024-07-18 12:41:03 +0200

    minor updates

--- CropExt_PC.R
+++ CropExt_PC.R
@@ -1,14 +1,13 @@
 library(terra)
-MI1 <- rast('I:/DATA/output/MI_MaxTOffset_V2.tif')
-MI2 <- rast('I:/DATA/output/MI_MinTOffset_V2.tif')
-MI3 <- rast('E:/Output/MicrorefugiaIndex/MI_WarmingMagnitude.tif')
+MI1 <- rast('E:/Output/MicrorefugiaIndex/Original_extNOTmatch/MI_BVoMC_75km_25m.tif')
+MI2 <- rast('E:/Output/MicrorefugiaIndex/Original_extNOTmatch/MI_MaxTOffset.tif')
 
+cropext <- ext(MI1)
 
-
-cropext <- ext(MI3)
-
-MI1crop <- crop(MI1, cropext)
-writeRaster(MI1crop, filename = 'E:/Output/MicrorefugiaIndex/MI_MaxTOffset_V2_cropext.tif', overwrite=T)
+terraOptions(verbose=T)
+MI2crop <- crop(MI2, cropext) #crop extent.
+terraOptions(verbose=T)
+writeRaster(MI2crop, filename = 'E:/Output/MicrorefugiaIndex/Original_extNOTmatch/MI_MaxTOffset_V2_cropext.tif', overwrite=T)
 print(MI1crop)
 
 MI2_round <- round(MI2, digits = 1)
