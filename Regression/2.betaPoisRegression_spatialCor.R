1e719aa54bbb038167c69e889b3242349e6e37ca
XiaquZ <xiaqu.zhou@gmail.com>
2025-01-20 18:17:01 +0100

    Rename the R scripts and add the residual test for both beta and poisson models.

--- Regression/2.betaPoisRegression_spatialCor.R
+++ Regression/2.betaPoisRegression_spatialCor.R
@@ -28,8 +28,7 @@ predict_s$MF_av <- as.numeric(unlist(predict_s$MF_av))
 predict_s$MF_0.8T <- as.numeric(unlist(predict_s$MF_0.8T))
 predict_s <- as_tibble(predict_s)
 predict_s
-predict_s$MF_av[which(predict_s$MF_av == 0)] <- 0.0001
-predict_s$MF_av[which(predict_s$MF_av == 1)] <- 0.9999
+
 # data_sampled$pos <- numFactor(data_sampled$x, data_sampled$y)
 # data_sampled$group <- factor(rep(1, nrow(data_sampled)))
 class(predict_s$type)
@@ -37,137 +36,72 @@ predict_s$type[which(predict_s$type == 1)] <- "Broadleaved forest"
 predict_s$type[which(predict_s$type == 2)] <- "Coniferous forest"
 predict_s$type <- as.factor(predict_s$type)
 
+hist(predict_s$MF_0.8T)
+hist(predict_s$MF_av)
+
 #### Poisson model####
 # Poisson WITHOUT intercept.
-mod_pois <- gam(
-  MF_singleT ~ type + coast + cover + elevation + eastness +
-    northness + relative_elevation + slope + TWI +
+mod_pois_no_intercept<- gam(
+  MF_0.8T ~ northness * cover * slope +
+    cover * type +
+    coast +
+    elevation +
+    relative_elevation +
+    TWI +
     s(x, y, bs = "gp", m = 2) - 1,
-  family = poisson(link = "log"),
-  data = data_sample.mf02
-)
-summary(mod_pois)
-simulationOutput <- simulateResiduals(fittedModel = mod_pois)
-plot(simulationOutput)
-testOutliers(simulationOutput, type = "bootstrap") # outliers are not significant.
-plotResiduals(simulationOutput, form = data_1000$coast)
-plotResiduals(simulationOutput, form = data_1000$cover) # Looks better
-plotResiduals(simulationOutput, form = data_sampled$elevation)
-plotResiduals(simulationOutput, form = data_sampled$eastness)
-plotResiduals(simulationOutput, form = data_sampled$northness)
-plotResiduals(simulationOutput, form = data_sampled$relative_elevation)
-plotResiduals(simulationOutput, form = data_sampled$slope)
-
-# Poisson with intercept
-mod_pois_intercep <- gam(
-  MF_0.8T ~ type + cover + coast + elevation + relative_elevation +
-    slope + eastness + northness + TWI +
-    s(x, y, bs = "gp", m = 2),
   family = poisson(link = "log"),
   data = predict_s
 )
-summary(mod_pois_intercep)
-simulationOutput <- simulateResiduals(fittedModel = mod_pois_intercep)
-plot(simulationOutput)
-save(mod_pois_intercep,
-  file = "I:/DATA/output/MF_origi/Poisson_MF_singleT_orig.RData"
-)
+summary(mod_pois_no_intercept)
+simulationOutput_pois <- simulateResiduals(fittedModel = mod_pois_no_intercept)
+plot(simulationOutput_pois)
+testDispersion(simulationOutput_pois) # overdispersion
+par(mfrow = c(1,2))
+plotResiduals(simulationOutput_pois, predict_s$relative_elevation)
+plotResiduals(simulationOutput_pois, predict_s$elevation)
+plotResiduals(simulationOutput_pois, predict_s$type)
+# Overdispersion caused by elevation and forest types?
 
-#### Use 'gam()' for faster beta regression accounting spatial autocorrelation.####
+#### Use 'gam()' for faster 
+####beta regression accounting spatial autocorrelation.
+# Let the minimum value be 0.0001 and the maximum value be 0.9999.
+predict_s$MF_av[which(predict_s$MF_av == 0)] <- 0.0001
+predict_s$MF_av[which(predict_s$MF_av == 1)] <- 0.9999
+
 # beta WITHOUT intercept.
 mod_beta <- gam(
-  MF_av ~ type + coast + cover + elevation + eastness +
-    northness + relative_elevation + slope + TWI +
+  MF_av ~ northness * cover * slope +
+    cover * type +
+    coast +
+    elevation +
+    relative_elevation +
+    TWI +
     s(x, y, bs = "gp", m = 2) - 1,
-  family = betar(link = "logit"),
-  data = data_1000
-)
-summary(mod_beta)
-simulationOutput <- simulateResiduals(fittedModel = mod_beta)
-plot(simulationOutput)
-
-# beta with intercept
-mod_beta_intercep <- gam(
-  MF_av ~   type + cover + coast + elevation + relative_elevation +
-    slope + eastness + northness + TWI +
-    s(x, y, bs = "gp", m = 2),
   family = betar(link = "logit"),
   data = predict_s
 )
-summary(mod_beta_intercep)
-simulationOutput <- simulateResiduals(fittedModel = mod_beta_intercep)
-plot(simulationOutput)
-save(mod_beta_intercep, file = "I:/DATA/output/MF_origi/MF_avBeta_orig.RData")
+summary(mod_beta)
+simulationOutput_beta <- simulateResiduals(fittedModel = mod_beta)
+plot(simulationOutput_beta)
+testDispersion(simulationOutput_beta) # overdispersion
+testZeroInflation(simulationOutput_beta)
+plotResiduals(simulationOutput_beta, predict_s$relative_elevation)
+plotResiduals(simulationOutput_beta, predict_s$elevation)
+plotResiduals(simulationOutput_beta, predict_s$type)
+plotResiduals(simulationOutput_beta, predict_s$slope)
 
-#### Test the predictor saperately####
-# for cover
-mod_cover <- gam(
-  MF_av ~ type + coast + s(cover) + elevation + eastness +
-    northness + relative_elevation + slope + TWI +
-    s(x, y, bs = "gp", m = 2) - 1,
-  family = betar(link = "logit"),
-  data = data_sampled
+# plot the interaction.
+library(ggeffects)
+cover_type <- predict_response(
+    mf_model, c("cover", "type"),
+    margin = "mean_mode"
 )
-summary(mod_cover)
-simulationOutput <- simulateResiduals(fittedModel = mod_cover)
-plot(simulationOutput)
+cover_type
+plot(cover_type)
 
-# For elevation
-mod_elevation <- gam(
-  MF_av ~ type + coast + cover + s(elevation) + eastness +
-    northness + relative_elevation + slope + TWI +
-    s(x, y, bs = "gp", m = 2) - 1,
-  family = betar(link = "logit"),
-  data = data_sampled
+slo_north_cov <- predict_response(
+    mf_model, c("cover", "northness [-1, 0, 1]", "slope[0.0, 45.0]"),
+    margin = "mean_mode" # !Check if "mean_mode" is corret here.
 )
-summary(mod_elevation)
-simulationOutput <- simulateResiduals(fittedModel = mod_elevation)
-plot(simulationOutput)
-
-# eastness
-mod_east <- gam(
-  MF_av ~ type + coast + cover + elevation + s(eastness) +
-    northness + relative_elevation + slope + TWI +
-    s(x, y, bs = "gp", m = 2) - 1,
-  family = betar(link = "logit"),
-  data = data_sampled
-)
-summary(mod_east)
-simulationOutput <- simulateResiduals(fittedModel = mod_east)
-plot(simulationOutput)
-
-# for northness
-mod_north <- gam(
-  MF_av ~ type + coast + cover + elevation + eastness +
-    s(northness) + relative_elevation + slope + TWI +
-    s(x, y, bs = "gp", m = 2) - 1,
-  family = betar(link = "logit"),
-  data = data_sampled
-)
-summary(mod_north)
-simulationOutput <- simulateResiduals(fittedModel = mod_north)
-plot(simulationOutput)
-
-# reletive elevation
-mod_re_elev <- gam(
-  MF_av ~ type + coast + cover + elevation + eastness +
-    northness + s(relative_elevation) + slope + TWI +
-    s(x, y, bs = "gp", m = 2) - 1,
-  family = betar(link = "logit"),
-  data = data_sampled
-)
-summary(mod_re_elev)
-simulationOutput <- simulateResiduals(fittedModel = mod_re_elev)
-plot(simulationOutput)
-
-# slope
-mod_slope <- gam(
-  MF_av ~ type + coast + cover + elevation + eastness +
-    northness + relative_elevation + s(slope) + TWI +
-    s(x, y, bs = "gp", m = 2) - 1,
-  family = betar(link = "logit"),
-  data = data_sampled
-)
-summary(mod_slope)
-simulationOutput <- simulateResiduals(fittedModel = mod_slope)
-plot(simulationOutput)
+slo_north_cov
+plot(slo_north_cov)
