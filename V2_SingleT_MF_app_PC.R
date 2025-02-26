e7d8d6fe7178f949da38e1391e3c0dd48b3a1fe3
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-26 14:18:15 +0100

    update function.

--- V2_SingleT_MF_app_PC.R
+++ V2_SingleT_MF_app_PC.R
@@ -1,35 +1,38 @@
 library(terra)
 ## Load all the rasters after cropping to the same extent.
-mi_warming <- rast("E:/Output/MicrorefugiaIndex/MI_WarmingMagnitude.tif")
-mi_max <- rast("E:/Output/MicrorefugiaIndex/Offset/MI_MaxTOffset_V2_crop.tif")
-mi_min <- rast("E:/Output/MicrorefugiaIndex/Offset/MI_MinTOffset_V2_crop.tif")
-mi_fvomc <- rast("E:/Output/MicrorefugiaIndex/VoCC/MI_fvocc_75km_25m_add0.tif")
-mi_bvomc <- rast("E:/Output/MicrorefugiaIndex/VoCC/MI_bvocc_75km_25m_add0.tif")
+mi_warming <- rast("H:/Output/MicrorefugiaIndex/MI_WarmingMagnitude.tif")
+mi_max <- rast("H:/Output/MicrorefugiaIndex/Offset/MI_MaxTOffset_V2_crop.tif")
+mi_min <- rast("H:/Output/MicrorefugiaIndex/Offset/MI_MinTOffset_V2_crop.tif")
+mi_fvomc <- rast("H:/Output/MicrorefugiaIndex/VoCC/MI_fvocc_75km_25m_add0.tif")
+mi_bvomc <- rast("H:/Output/MicrorefugiaIndex/VoCC/MI_bvocc_75km_25m_add0.tif")
 
 terraOptions(verbose = T) ## monitor the memory
-crop_files <- lapply(filenames, rast)
+
 c_stack <- c(mi_bvomc, mi_fvomc, mi_max, mi_min, mi_warming)
 
 #### Use single threshold mutifunctionality to count the MIs that beyond
 #### the threshold values in each cell.####
 # use parallel with app() from terra package to sum the number of MIs
 # greater than threshold.
-parallel_fun <- function(i) {
-    sum(i > 0.6)
+# Define a function to count the number of MIs greater than a threshold
+parallel_fun <- function(i, threshold = 0.6) {
+  sum(i > threshold)
 }
 
-terraOptions(verbose = T)
-MF_singleT <- app(c_stack,
-    fun = function(i, ff) ff(i),
-    cores = 10,
-    ff = parallel_fun
+# Apply the function in parallel using the app() function from terra
+MF_singleT <- app(
+  c_stack,
+  fun = function(i, ff, ...) ff(i, ...),
+  cores = 10,
+  ff = parallel_fun,
+  threshold = 0.6
 )
 # with core>1, things can be speed up.
 plot(MF_singleT)
 
 terraOptions(verbose = T)
 writeRaster(MF_singleT,
-    filename = "E:/Output/Multifunctionality/MF_singleThreshold0.6_5MIs_EU_25m_EPSG3035.tif",
+    filename = "H:/Output/Multifunctionality/MF_singleThreshold0.6_5MIs_EU_25m_EPSG3035.tif",
     overwrite = TRUE
 )
-names(MF_singleT) <- "MF_singleT_5MIs"
+names(MF_singleT) <- "MF_singleT0.6_5MIs"
